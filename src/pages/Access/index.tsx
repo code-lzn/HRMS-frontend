32a8f06cb727fc01aadfa57b2d9ad2ad45164bd6
18337713619 <3219085070@qq.com>
2026-07-09 16:47:36 +0800

    添加组件

--- src/pages/Access/index.tsx
+++ src/pages/Access/index.tsx
@@ -1,9 +1,14 @@
+import { healthUsingGet } from '@/api/testHealth';
 import { PageContainer } from '@ant-design/pro-components';
 import { Access, useAccess } from '@umijs/max';
 import { Button } from 'antd';
 
 const AccessPage: React.FC = () => {
   const access = useAccess();
+  const test = async () => {
+    const res = await healthUsingGet();
+    console.log(res);
+  };
   return (
     <PageContainer
       ghost
@@ -13,6 +18,7 @@ const AccessPage: React.FC = () => {
     >
       <Access accessible={access.canSeeAdmin}>
         <Button>只有 Admin 可以看到这个按钮</Button>
+        <Button onClick={() => test()}>测试接口</Button>
       </Access>
     </PageContainer>
   );
